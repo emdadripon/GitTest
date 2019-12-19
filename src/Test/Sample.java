59bd45c57caf34fc8bd09ea81be3c82af575cb90
emdadripon <eriponctg@gmail.com>
2019-12-18 21:12:53 -0500

    git commit -m "Test change second"

--- src/Test/Sample.java
+++ src/Test/Sample.java
@@ -4,20 +4,16 @@ import java.util.Scanner;
 
 public class Sample {
 
-    public long doInvert(long number){
-        long invert = 0;
-        while(number != 0){
-            invert = (invert * 10) + (number % 10);
-            number = number / 10;
-        }
-        return invert;
-    }
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Number to Reverse:-= ");
-        int lnum = scan.nextInt();
-        Sample s = new Sample();
-        System.out.println("Input value: " + lnum);
-        System.out.println("Invert value: " + s.doInvert(lnum));
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(" a" + a);
+        System.out.println("b " + b);
     }
 }
